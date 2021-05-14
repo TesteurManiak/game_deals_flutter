2553186847e6c25f64bcb1afff38e7da34d34ffd
Guillaume Roux <rouxguillaume8@gmail.com>
2021-05-14 16:04:27 +0200

    use of an enum for filters actions

--- lib/ui/search_page/search_page.dart
+++ lib/ui/search_page/search_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:maniak_game_deals/bloc/bloc_provider.dart';
 import 'package:maniak_game_deals/bloc/games_bloc.dart';
+import 'package:maniak_game_deals/models/filters_actions_enum.dart';
 import 'package:maniak_game_deals/models/game_model.dart';
 import 'package:maniak_game_deals/ui/common/responsive.dart';
 import 'package:maniak_game_deals/ui/search_page/widgets/filters_dialog.dart';
@@ -19,12 +20,18 @@ class _SearchPageState extends State<SearchPage> {
   late final _gamesBloc = BlocProvider.of<GamesBloc>(context);
 
   void _openFiltersDialog() {
-    showDialog<bool>(context: context, builder: (_) => FiltersDialog())
-        .then((value) {
-      if (value == null || !value) {
-        print('Dont update filters');
-      } else {
-        print('Update filters');
+    showDialog<FiltersActions>(
+        context: context, builder: (_) => FiltersDialog()).then((value) {
+      switch (value) {
+        case FiltersActions.filters:
+          print('Update filters');
+          break;
+        case FiltersActions.reset:
+          print('Reset filters');
+          break;
+        default:
+          print('Dont update filters');
+          break;
       }
     });
   }
--- lib/ui/search_page/widgets/filters_dialog.dart
+++ lib/ui/search_page/widgets/filters_dialog.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:maniak_game_deals/models/filters_actions_enum.dart';
 import 'package:maniak_game_deals/ui/search_page/widgets/descendant.dart';
 import 'package:maniak_game_deals/ui/search_page/widgets/results_number_dropdown_btn.dart';
 import 'package:maniak_game_deals/ui/search_page/widgets/sort_by_dropdown_btn.dart';
@@ -11,6 +12,13 @@ class FiltersDialog extends StatefulWidget {
 
 class _FiltersDialogState extends State<FiltersDialog> {
   final _formKey = GlobalKey<FormState>();
+
+  void _filters() {
+    final formState = _formKey.currentState;
+    if (formState != null && formState.validate()) {
+      Navigator.pop(context, FiltersActions.filters);
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -31,17 +39,16 @@ class _FiltersDialogState extends State<FiltersDialog> {
         ),
       ),
       actions: [
+        IconButton(
+          icon: const Icon(Icons.refresh),
+          onPressed: () => Navigator.pop(context, FiltersActions.reset),
+        ),
         TextButton(
-          onPressed: () => Navigator.pop(context, false),
+          onPressed: () => Navigator.pop(context, FiltersActions.cancel),
           child: const Text('Cancel'),
         ),
         TextButton(
-          onPressed: () {
-            final formState = _formKey.currentState;
-            if (formState != null && formState.validate()) {
-              Navigator.pop(context, true);
-            }
-          },
+          onPressed: _filters,
           child: const Text('Filter'),
         ),
       ],
